21ee87fb3fb55cfc7c8b8d8b407e6327bb513090
Lance Bailey <lancebailey222@gmail.com>
2021-05-26 13:11:07 -0400

    started playing with jquery

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,57 @@
-var clock = document.getElementById("currentDay");
-var saveButton09 = document.getElementById("save09");
-var saveButton10 = document.getElementById("save10");
+var clock = $('#currentDay');
+// var saveButton09 = document.getElementById("save09");
+// var saveButton10 = document.getElementById("save10");
+
+
+// var elevenAM = $('#jobInput11').val();
+// var twelvePM = $('#jobInput12').val();
+// var onePM = $('#jobInput13').val();
+// var twoPM = $('#jobInput14').val();
+// var threePM = $('#jobInput15').val();
+// var fourPM = $('#jobInput16').val();
+// var fivePM = $('#jobInput17').val();
 // var jobText = document.getElementById("jobInput");
 // var timeSlot = document.getElementById("timeslot")
-var jobDescr = [] || JSON.parse(localStorage.getItem("jobDescr")) ;
+// var jobDescr = [] || JSON.parse(localStorage.getItem("jobDescr")) ;
 
-function updateClock(){
-    clock.textContent ="It is currently " + moment().format('dddd MMMM Do YYYY, h:mm:ss a');
-}
-setInterval(updateClock, 1000);
+setInterval(function (){
+    clock.text('It is currently ' + moment().format('dddd MMMM Do YYYY, h:mm:ss a'));
+    }, 1000)
 
-function saveText09(){
-    var inputEl = document.getElementById("jobInput09");
-    var input = inputEl.value;
-    var timeEl = document.getElementById("timeslot09");
-    var time = timeEl.textContent;
-    var jobObj = {
-        time: time,
-        job: input,
-    }
-    console.log(jobObj);
-    jobDescr.pop(jobObj);
-    localStorage.setItem("jobDescr", JSON.stringify(jobObj));
-}
-function saveText10(){
-    var inputEl = document.getElementById("jobInput10");
-    var input = inputEl.value;
-    var timeEl = document.getElementById("timeslot10");
-    var time = timeEl.textContent;
-    var jobObj = {
-        time: time,
-        job: input,
-    }
-    console.log(jobDescr);
-    jobDescr.pop(jobObj);
-    localStorage.setItem("jobDescr", JSON.stringify(jobObj));
-}
+ 
+$('#jobInput09').val(localStorage.nineAM);
 
+// function saveText09(){
+//     // var inputEl = $('#jobInput09');
+//     // var nineAM = $('#jobInput09').val();
+//     // var timeEl = 
+//     // var time = $('#timeslot09').text();
+//     // var nineAM = input;
+//     var nineAM = $('#jobInput09').val();
+//     var nineAMData = {
+//         time: "9AM",
+//         job: nineAM,
+//     }
+//     console.log(nineAMData);
+//     jobDescr.push(nineAMData);
+//     localStorage.setItem("jobDescr", JSON.stringify(jobDescr));
+// }
+// function saveText10(){
+//     // var inputEl = document.getElementById("jobInput10");
+//     // var input = inputEl.value;
+//     // var timeEl = document.getElementById("timeslot10");
+//     // var time = timeEl.textContent;
+//     var tenAM = $('#jobInput10').val();
+//     var tenAMData = {
+//         time: "10AM",
+//         job: tenAM,
+//     }
+//     console.log(tenAMData);
+//     jobDescr.push(tenAMData);
+//     localStorage.setItem("jobDescr", JSON.stringify(jobDescr));
+// }
 
-saveButton09.addEventListener("click", saveText09);
-saveButton10.addEventListener("click", saveText10);
+$('#save09').on('click',function(){
+    localStorage.nineAM = $('#jobInput09').val();
+});
+// saveButton10.addEventListener("click", saveText10);
