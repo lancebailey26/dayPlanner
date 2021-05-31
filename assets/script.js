1cbe83b0140d508ca9636fbabe582397742ac8f5
Lance Bailey <lancebailey222@gmail.com>
2021-05-31 12:02:59 -0400

    time colorization working

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,14 @@ var currentTime = moment().format('H');
 // var saveButton09 = document.getElementById("save09");
 // var saveButton10 = document.getElementById("save10");
 
-// var nineAMSLOT = $('#timeslot09');
+var int9am = 9;
+var int10am = 10;
+var int11am = 11;
+var int12pm = 12;
+var int1pm = 13;
+var int2pm = 14;
+var int3pm = 15;
+var int4pm = 16;
 // var tenAMSLOT = $('#save10');
 // var elevenAMSLOT = $('#timeslot11');
 // var twelvePMSLOT = $('#timeslot12');
@@ -11,10 +18,12 @@ var currentTime = moment().format('H');
 // var twoPMSLOT = $('#timeslot14');
 // var threePMSLOT = $('#timeslot15');
 // var fourPMSLOT = $('#timeslot16');
-// var fivePMSLOT = $('#timeslot17');
+// var fivePMInput = document.querySelector('#timeslot17').textContent;
+// fivePMInput = parseInt(fivePMInput[0,1]);
+var int5pm = 17;
 // var tenAMINPUT = $('#jobinput10');
-var anyButton = $('button');
-var anyInput = $('input');
+// var anyButton = $('button');
+// var anyInput = $('.input');
 // var jobText = document.getElementById("jobInput");
 // var timeSlot = document.getElementById("timeslot")
 // var jobDescr = [] || JSON.parse(localStorage.getItem("jobDescr")) ;
@@ -95,24 +104,84 @@ $('#save17').on('click',function(){
 
 // console.log(tenAMSLOT.val());
 console.log(currentTime);
+// console.log(fivePMInput);
 
-if (currentTime > anyButton.val()){
-    anyInput.addClass("past");
-    anyInput.removeClass("future");
-    anyInput.removeClass("present")
+// console.log(anyInput.val());
+
+// if (currentTime >  anyInput.val()){
+//     anyInput.addClass("present");
+// }
+
+if (int9am < currentTime){
+    $('#jobInput09').addClass('past');
+}else if (int9am > currentTime){
+    $('#jobInput09').addClass('future');
+}else if (int9am == currentTime){
+    $('#jobInput09').addClass('present');
 }
 
-if (currenTime = anyButton.val()){
-    anyInput.removeClass("past");
-    anyInput.removeClass("future");
-    anyInput.addClass("present");
+if (int10am < currentTime){
+    $('#jobInput10').addClass('past');
+}else if (int10am > currentTime){
+    $('#jobInput10').addClass('future');
+}else if (int10am == currentTime){
+    $('#jobInput10').addClass('present');
 }
 
-if (currentTime < anyButton.val()){
-    anyInput.addClass("future");
-    anyInput.removeClass("past");
-    anyInput.removeClass("present");
+if (int11am < currentTime){
+    $('#jobInput11').addClass('past');
+}else if (int11am > currentTime){
+    $('#jobInput11').addClass('future');
+}else if (int11am = currentTime){
+    $('#jobInput11').addClass('present');
 }
 
+if (int12pm < currentTime){
+    $('#jobInput12').addClass('past');
+}else if (int12pm > currentTime){
+    $('#jobInput12').addClass('future');
+}else if (int12pm = currentTime){
+    $('#jobInput12').addClass('present');
+}
 
+if (int1pm < currentTime){
+    $('#jobInput13').addClass('past');
+}else if (int1pm > currentTime){
+    $('#jobInput13').addClass('future');
+}else if (int1pm = currentTime){
+    $('#jobInput13').addClass('present');
+}
+
+if (int2pm < currentTime){
+    $('#jobInput14').addClass('past');
+}else if (int2pm > currentTime){
+    $('#jobInput14').addClass('future');
+}else if (int2pm = currentTime){
+    $('#jobInput14').addClass('present');
+}
+
+if (int3pm < currentTime){
+    $('#jobInput15').addClass('past');
+}else if (int3pm > currentTime){
+    $('#jobInput15').addClass('future');
+}else if (int3pm = currentTime){
+    $('#jobInput15').addClass('present');
+}
+
+if (int4pm < currentTime){
+    $('#jobInput16').addClass('past');
+}else if (int4pm > currentTime){
+    $('#jobInput16').addClass('future');
+}else if (int4pm = currentTime){
+    $('#jobInput16').addClass('present');
+}
+
+if (int5pm < currentTime){
+    $('#jobInput17').addClass('past');
+}else if (int5pm > currentTime){
+    $('#jobInput17').addClass('future');
+}else if (int5pm == currentTime){
+    $('#jobInput17').addClass('present');
+}
+// if ()
 // saveButton10.addEventListener("click", saveText10);
